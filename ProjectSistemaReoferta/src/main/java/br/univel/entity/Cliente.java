a011fac165b2aeffa6540703d8540f54b99811d2
Felipe Baccin <felipebaccincvel@gmail.com>
2015-11-25 14:28:41 -0200

    cirando os paranaue da entidade e as enum

--- ProjectSistemaReoferta/src/main/java/br/univel/entity/Cliente.java
+++ ProjectSistemaReoferta/src/main/java/br/univel/entity/Cliente.java
@@ -1,5 +1,75 @@
 package br.univel.entity;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import br.univel.Enum.Estado;
+import br.univel.Enum.Genero;
+
 public class Cliente {
 
+	private int id;
+	private String nome;
+	private String telefone;
+	private String endereco;
+	private String cidade;
+	private String email;
+	
+	//enum
+	@Enumerated(EnumType.STRING)
+	Estado estado;
+	@Enumerated(EnumType.STRING)
+	Genero genero;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	public Genero getGenero() {
+		return genero;
+	}
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+	
+	
 }
--- ProjectSistemaReoferta/src/main/java/br/univel/entity/Usuario.java
+++ ProjectSistemaReoferta/src/main/java/br/univel/entity/Usuario.java
@@ -1,5 +1,31 @@
 package br.univel.entity;
 
 public class Usuario {
-
+	
+	private int id;
+	private String senha;
+	private Cliente cliente;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	
+	
 }
