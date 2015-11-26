52d90d0afbd13ae4863802365a08b4d11adbab28
Felipe Baccin <felipebaccincvel@gmail.com>
2015-11-26 17:06:16 -0200

    modelo cliente

--- ProjectSistemaReoferta/src/main/java/br/univel/modelo/ModeloCliente.java
+++ ProjectSistemaReoferta/src/main/java/br/univel/modelo/ModeloCliente.java
@@ -1,22 +1,51 @@
 package br.univel.modelo;
 
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
+
+import br.univel.entity.Cliente;
 
 public class ModeloCliente extends AbstractTableModel {
 
+	List<Cliente> listaClientes;
+
+	public void ModelCliente(List<Cliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 2;
 	}
 
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return listaClientes.size();
 	}
 
 	public Object getValueAt(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
+		Cliente cliente = listaClientes.get(arg0);
+
+		switch (arg1) {
+		case 0:
+			return cliente.getId();
+		case 1:
+			return cliente.getNome();
+		default:
+			return "Erro";
+		}
 	}
+
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0:
+			return "ID";
+		case 1:
+			return "Nome";
+		default:
+			return "Erro";
+		}
+
+	};
 
 }
