a6c48f09c9f7425d1b3461724df121001bcef9b8
Felipe Baccin <felipebaccincvel@gmail.com>
2015-11-26 16:05:11 -0200

    classe de conexão

--- ProjectSistemaReoferta/src/main/java/br/univel/DAO/Conexao.java
+++ ProjectSistemaReoferta/src/main/java/br/univel/DAO/Conexao.java
@@ -1,12 +1,47 @@
 package br.univel.DAO;
 
-import javax.persistence.EntityManagerFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
 
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
 
-public class Conexao {
+import org.jboss.logging.Logger;
 
-	private static EntityManagerFactory factory;
+public class Conexao {
 	
+	private static final String DRIVER = "org.postgres.jdbc.Driver";
+	private static final String URL = "jdbc:postgres://localhost:8400/projectSistemaReoferta";
+	private static final String SENHA = "postgres";
+	private static final String USUARIO = "postgres";
+	
+	public static Connection conectar(){
+		try {
+			
+			String url = URL;
+			String usuario = USUARIO;
+			String senha = SENHA;
+			
+			Class.forName("org.postgresql.Driver");
+			
+			Connection con = null;
+			
+			con = DriverManager.getConnection(url,usuario,senha);
+			
+			System.out.println("Conexão realizada com sucesso");
+			
+			DriverManager.registerDriver(new org.postgresql.Driver());
+			
+			return con;		
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (SQLException e1) {
+			Logger.getLogger(Conexao.class.getName()).log(null, null, Level.SEVERE, null,e1);;
+		}
+		return null;
+	}
 	
 }
