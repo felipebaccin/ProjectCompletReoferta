a52a34998d5aae128d56400ef09d438749b1a65a
Felipe Baccin <felipebaccincvel@gmail.com>
2015-12-05 08:02:50 -0200

    ajeitando o resto mais ainda esta dando pau no produto e cliente em questão da combobox como se a mesma não estivesse pegando algum valor e assim disparando um null pointer exption

--- ProjectSistemaReoferta/src/main/java/br/univel/Tela/TelaVenda.java
+++ ProjectSistemaReoferta/src/main/java/br/univel/Tela/TelaVenda.java
@@ -75,15 +75,15 @@ public class TelaVenda extends JDialog {
 				
 			}
 		});
-		setBounds(100, 100, 450, 631);
+		setBounds(100, 100, 661, 633);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
 		gbl_contentPanel.columnWidths = new int[]{424, 0};
-		gbl_contentPanel.rowHeights = new int[]{221, 0, 0, 42, 14, 34, 92, 35, 0};
+		gbl_contentPanel.rowHeights = new int[]{128, 0, 0, 14, 34, 92, 35, 0};
 		gbl_contentPanel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		contentPanel.setLayout(gbl_contentPanel);
 		{
 			JPanel panel = new JPanel();
@@ -95,16 +95,16 @@ public class TelaVenda extends JDialog {
 			gbc_panel.gridy = 0;
 			contentPanel.add(panel, gbc_panel);
 			GridBagLayout gbl_panel = new GridBagLayout();
-			gbl_panel.columnWidths = new int[]{424, 0};
-			gbl_panel.rowHeights = new int[]{14, 20, 14, 20, 14, 20, 14, 20, 14, 46, 0};
-			gbl_panel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-			gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_panel.columnWidths = new int[]{158, 0, 0};
+			gbl_panel.rowHeights = new int[]{14, 14, 14, 14, 14, 0};
+			gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+			gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 			panel.setLayout(gbl_panel);
 			{
 				JLabel lblNmeroTransao = new JLabel("N\u00DAMERO");
 				GridBagConstraints gbc_lblNmeroTransao = new GridBagConstraints();
 				gbc_lblNmeroTransao.anchor = GridBagConstraints.WEST;
-				gbc_lblNmeroTransao.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNmeroTransao.insets = new Insets(0, 0, 5, 5);
 				gbc_lblNmeroTransao.gridx = 0;
 				gbc_lblNmeroTransao.gridy = 0;
 				panel.add(lblNmeroTransao, gbc_lblNmeroTransao);
@@ -114,8 +114,8 @@ public class TelaVenda extends JDialog {
 				GridBagConstraints gbc_txtNumeroTransacao = new GridBagConstraints();
 				gbc_txtNumeroTransacao.fill = GridBagConstraints.HORIZONTAL;
 				gbc_txtNumeroTransacao.insets = new Insets(0, 0, 5, 0);
-				gbc_txtNumeroTransacao.gridx = 0;
-				gbc_txtNumeroTransacao.gridy = 1;
+				gbc_txtNumeroTransacao.gridx = 1;
+				gbc_txtNumeroTransacao.gridy = 0;
 				panel.add(txtNumeroTransacao, gbc_txtNumeroTransacao);
 				txtNumeroTransacao.setColumns(10);
 			}
@@ -123,9 +123,9 @@ public class TelaVenda extends JDialog {
 				JLabel lblNewLabel = new JLabel("STATUS");
 				GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 				gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-				gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 				gbc_lblNewLabel.gridx = 0;
-				gbc_lblNewLabel.gridy = 2;
+				gbc_lblNewLabel.gridy = 1;
 				panel.add(lblNewLabel, gbc_lblNewLabel);
 			}
 			{
@@ -135,8 +135,8 @@ public class TelaVenda extends JDialog {
 				GridBagConstraints gbc_txtAndamento = new GridBagConstraints();
 				gbc_txtAndamento.fill = GridBagConstraints.HORIZONTAL;
 				gbc_txtAndamento.insets = new Insets(0, 0, 5, 0);
-				gbc_txtAndamento.gridx = 0;
-				gbc_txtAndamento.gridy = 3;
+				gbc_txtAndamento.gridx = 1;
+				gbc_txtAndamento.gridy = 1;
 				panel.add(txtAndamento, gbc_txtAndamento);
 				txtAndamento.setColumns(10);
 			}
@@ -144,9 +144,9 @@ public class TelaVenda extends JDialog {
 				JLabel lblDataHora = new JLabel("DATA / HORA FINALIZA\u00C7\u00C3O");
 				GridBagConstraints gbc_lblDataHora = new GridBagConstraints();
 				gbc_lblDataHora.anchor = GridBagConstraints.WEST;
-				gbc_lblDataHora.insets = new Insets(0, 0, 5, 0);
+				gbc_lblDataHora.insets = new Insets(0, 0, 5, 5);
 				gbc_lblDataHora.gridx = 0;
-				gbc_lblDataHora.gridy = 4;
+				gbc_lblDataHora.gridy = 2;
 				panel.add(lblDataHora, gbc_lblDataHora);
 			}
 			{
@@ -155,8 +155,8 @@ public class TelaVenda extends JDialog {
 				GridBagConstraints gbc_txtDataHoraFinalizacao = new GridBagConstraints();
 				gbc_txtDataHoraFinalizacao.fill = GridBagConstraints.HORIZONTAL;
 				gbc_txtDataHoraFinalizacao.insets = new Insets(0, 0, 5, 0);
-				gbc_txtDataHoraFinalizacao.gridx = 0;
-				gbc_txtDataHoraFinalizacao.gridy = 5;
+				gbc_txtDataHoraFinalizacao.gridx = 1;
+				gbc_txtDataHoraFinalizacao.gridy = 2;
 				panel.add(txtDataHoraFinalizacao, gbc_txtDataHoraFinalizacao);
 				txtDataHoraFinalizacao.setColumns(10);
 			}
@@ -164,9 +164,9 @@ public class TelaVenda extends JDialog {
 				JLabel lblCdigoCliente = new JLabel("C\u00D3DIGO CLIENTE");
 				GridBagConstraints gbc_lblCdigoCliente = new GridBagConstraints();
 				gbc_lblCdigoCliente.anchor = GridBagConstraints.WEST;
-				gbc_lblCdigoCliente.insets = new Insets(0, 0, 5, 0);
+				gbc_lblCdigoCliente.insets = new Insets(0, 0, 5, 5);
 				gbc_lblCdigoCliente.gridx = 0;
-				gbc_lblCdigoCliente.gridy = 6;
+				gbc_lblCdigoCliente.gridy = 3;
 				panel.add(lblCdigoCliente, gbc_lblCdigoCliente);
 			}
 			{
@@ -174,25 +174,26 @@ public class TelaVenda extends JDialog {
 				GridBagConstraints gbc_comboCliente = new GridBagConstraints();
 				gbc_comboCliente.fill = GridBagConstraints.HORIZONTAL;
 				gbc_comboCliente.insets = new Insets(0, 0, 5, 0);
-				gbc_comboCliente.gridx = 0;
-				gbc_comboCliente.gridy = 7;
+				gbc_comboCliente.gridx = 1;
+				gbc_comboCliente.gridy = 3;
 				panel.add(comboCliente, gbc_comboCliente);
 			}
 			{
 				JLabel lblNewLabel_1 = new JLabel("PRODUTO");
 				GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+				gbc_lblNewLabel_1.fill = GridBagConstraints.VERTICAL;
 				gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
-				gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+				gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
 				gbc_lblNewLabel_1.gridx = 0;
-				gbc_lblNewLabel_1.gridy = 8;
+				gbc_lblNewLabel_1.gridy = 4;
 				panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 			}
 			{
 				comboProduto = new JComboBox();
 				GridBagConstraints gbc_comboProduto = new GridBagConstraints();
-				gbc_comboProduto.fill = GridBagConstraints.HORIZONTAL;
-				gbc_comboProduto.gridx = 0;
-				gbc_comboProduto.gridy = 9;
+				gbc_comboProduto.anchor = GridBagConstraints.WEST;
+				gbc_comboProduto.gridx = 1;
+				gbc_comboProduto.gridy = 4;
 				panel.add(comboProduto, gbc_comboProduto);
 			}
 		}
@@ -217,51 +218,12 @@ public class TelaVenda extends JDialog {
 			txtQuantidade.setColumns(10);
 		}
 		{
-			JPanel panel = new JPanel();
-			GridBagConstraints gbc_panel = new GridBagConstraints();
-			gbc_panel.insets = new Insets(0, 0, 5, 0);
-			gbc_panel.fill = GridBagConstraints.BOTH;
-			gbc_panel.gridx = 0;
-			gbc_panel.gridy = 3;
-			contentPanel.add(panel, gbc_panel);
-			GridBagLayout gbl_panel = new GridBagLayout();
-			gbl_panel.columnWidths = new int[]{0, 0, 0};
-			gbl_panel.rowHeights = new int[]{35, 0};
-			gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-			gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-			panel.setLayout(gbl_panel);
-			{
-				JButton btnIncluir = new JButton("+");
-				btnIncluir.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent arg0) {
-						
-						if ((!comboProduto.getSelectedItem().toString().equals("")) && (!txtQuantidade.getText().equals(""))) {
-							adicionalistaItens();
-						}
-					}
-				});
-				GridBagConstraints gbc_btnIncluir = new GridBagConstraints();
-				gbc_btnIncluir.insets = new Insets(0, 0, 0, 5);
-				gbc_btnIncluir.gridx = 0;
-				gbc_btnIncluir.gridy = 0;
-				panel.add(btnIncluir, gbc_btnIncluir);
-			}
-			{
-				JButton btnExcluir = new JButton("-");
-				GridBagConstraints gbc_btnExcluir = new GridBagConstraints();
-				gbc_btnExcluir.gridx = 1;
-				gbc_btnExcluir.gridy = 0;
-				panel.add(btnExcluir, gbc_btnExcluir);
-			}
-		}
-		{
 			JLabel lblItens = new JLabel("ITENS");
 			GridBagConstraints gbc_lblItens = new GridBagConstraints();
 			gbc_lblItens.anchor = GridBagConstraints.WEST;
 			gbc_lblItens.insets = new Insets(0, 0, 5, 0);
 			gbc_lblItens.gridx = 0;
-			gbc_lblItens.gridy = 4;
+			gbc_lblItens.gridy = 3;
 			contentPanel.add(lblItens, gbc_lblItens);
 		}
 		{
@@ -270,11 +232,11 @@ public class TelaVenda extends JDialog {
 			gbc_panel.fill = GridBagConstraints.BOTH;
 			gbc_panel.insets = new Insets(0, 0, 5, 0);
 			gbc_panel.gridx = 0;
-			gbc_panel.gridy = 5;
+			gbc_panel.gridy = 4;
 			contentPanel.add(panel, gbc_panel);
 			GridBagLayout gbl_panel = new GridBagLayout();
 			gbl_panel.columnWidths = new int[]{0, 0};
-			gbl_panel.rowHeights = new int[]{115, 0};
+			gbl_panel.rowHeights = new int[]{273, 0};
 			gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
 			gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
 			panel.setLayout(gbl_panel);
@@ -297,7 +259,7 @@ public class TelaVenda extends JDialog {
 			gbc_panel.insets = new Insets(0, 0, 5, 0);
 			gbc_panel.fill = GridBagConstraints.BOTH;
 			gbc_panel.gridx = 0;
-			gbc_panel.gridy = 6;
+			gbc_panel.gridy = 5;
 			contentPanel.add(panel, gbc_panel);
 			GridBagLayout gbl_panel = new GridBagLayout();
 			gbl_panel.columnWidths = new int[]{0, 0, 0};
@@ -308,7 +270,7 @@ public class TelaVenda extends JDialog {
 			{
 				JLabel lblValorTotal = new JLabel("VALOR TOTAL");
 				GridBagConstraints gbc_lblValorTotal = new GridBagConstraints();
-				gbc_lblValorTotal.anchor = GridBagConstraints.EAST;
+				gbc_lblValorTotal.fill = GridBagConstraints.HORIZONTAL;
 				gbc_lblValorTotal.insets = new Insets(0, 0, 5, 5);
 				gbc_lblValorTotal.gridx = 0;
 				gbc_lblValorTotal.gridy = 0;
@@ -328,8 +290,8 @@ public class TelaVenda extends JDialog {
 			{
 				JLabel lblNewLabel_3 = new JLabel("VALOR PAGAMENTO");
 				GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+				gbc_lblNewLabel_3.fill = GridBagConstraints.HORIZONTAL;
 				gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-				gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
 				gbc_lblNewLabel_3.gridx = 0;
 				gbc_lblNewLabel_3.gridy = 1;
 				panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
@@ -347,7 +309,7 @@ public class TelaVenda extends JDialog {
 			{
 				JLabel lblValorTroco = new JLabel("VALOR TROCO");
 				GridBagConstraints gbc_lblValorTroco = new GridBagConstraints();
-				gbc_lblValorTroco.anchor = GridBagConstraints.EAST;
+				gbc_lblValorTroco.fill = GridBagConstraints.HORIZONTAL;
 				gbc_lblValorTroco.insets = new Insets(0, 0, 0, 5);
 				gbc_lblValorTroco.gridx = 0;
 				gbc_lblValorTroco.gridy = 2;
@@ -369,7 +331,7 @@ public class TelaVenda extends JDialog {
 			GridBagConstraints gbc_panel = new GridBagConstraints();
 			gbc_panel.fill = GridBagConstraints.BOTH;
 			gbc_panel.gridx = 0;
-			gbc_panel.gridy = 7;
+			gbc_panel.gridy = 6;
 			contentPanel.add(panel, gbc_panel);
 			GridBagLayout gbl_panel = new GridBagLayout();
 			gbl_panel.columnWidths = new int[]{0, 0, 0};
@@ -378,7 +340,7 @@ public class TelaVenda extends JDialog {
 			gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
 			panel.setLayout(gbl_panel);
 			{
-				JButton btnFinalizar = new JButton("FINALIZAR");
+				JButton btnFinalizar = new JButton("Fechar");
 				btnFinalizar.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						criarTransacao();
